d2d89c010ef2bd56462553fdc33c7ffe375e6124
Arthur Carabott <arthurc@gmail.com>
2017-05-09 23:26:09 +0100

    arduino for web

--- fader-test/fader-test.ino
+++ fader-test/fader-test.ino
@@ -51,25 +51,37 @@ void logValue(String& s, int value) {
 void loop() {
   g_prevLineValue = g_lineValue;
   g_lineValue = analogRead(g_line_pin);
+  Serial.print("l:");
+  Serial.println(g_lineValue);
 
-  faderMover.update(g_lineValue);
-  faderMover.isMoving() ? touchSensor.disable() : touchSensor.enable();
+  // faderMover.update(g_lineValue);
+  // faderMover.isMoving() ? touchSensor.disable() : touchSensor.enable();
   touchSensor.update();
 
-  const auto touching = touchSensor.isTouching();
-  // if (touchSensor.tapStarted()) { PL("touch start"); }
-  // if (touchSensor.tapEnded()) { PL("touch end"); }
+  // const auto touching = touchSensor.isTouching();
+  if (touchSensor.tapStarted()) { PL("m:touch start"); }
+  if (touchSensor.tapEnded()) { PL("m:touch end"); }
 
-  if (presets.isPreset(g_lineValue) &&
-      g_lineValue != g_prevLineValue &&
-      !g_tactileEnabled)
-  {
-    faderMover.tick(20);
-  }
+  // if (presets.isPreset(g_lineValue) &&
+  //     g_lineValue != g_prevLineValue &&
+  //     !g_tactileEnabled)
+  // {
+  //   faderMover.tick(20);
+  // }
 
   touche.update();
+
+  Serial.print("d:");
   touche.printResults();
 
+  // Serial.print("t:");
+  // Serial.print("0,");
+  // Serial.print(touchSensor.getTouchValue());
+  // Serial.print(",");
+  // Serial.print(touchSensor.getTouchThresh());
+  // Serial.print(",");
+  // Serial.print("1023");
+  // Serial.println("");
 
   // if (g_toucheTraining && !touche.training()) {
   //   g_toucheTraining = false;
@@ -93,48 +105,48 @@ void loop() {
   //   g_prevGesture = gesture;
   // }
 
-  const auto tapCount = touchSensor.tapCount();
+  // const auto tapCount = touchSensor.tapCount();
 
-  if (tapCount > 1) {
-    // P_LBL("tap count: ", tapCount);
-    auto success = false;
-    uint64_t delay = 150;
+  // if (tapCount > 1) {
+  //   // P_LBL("tap count: ", tapCount);
+  //   auto success = false;
+  //   uint64_t delay = 150;
 
-    if (tapCount == 2)      { success = presets.add(g_lineValue); }
-    else if (tapCount == 3) { success = presets.remove(g_lineValue); }
-    else if (tapCount == 4) {
-      success = presets.removeAll();
-      delay = 500;
-    }
-    else if (tapCount == 10) {
-      g_tactileEnabled = !g_tactileEnabled;
-      success = true;
-    }
+  //   if (tapCount == 2)      { success = presets.add(g_lineValue); }
+  //   else if (tapCount == 3) { success = presets.remove(g_lineValue); }
+  //   else if (tapCount == 4) {
+  //     success = presets.removeAll();
+  //     delay = 500;
+  //   }
+  //   else if (tapCount == 10) {
+  //     g_tactileEnabled = !g_tactileEnabled;
+  //     success = true;
+  //   }
 
-    if (success) {
-      faderMover.feedback(g_lineValue, 100, delay);
-    }
-  }
+  //   if (success) {
+  //     faderMover.feedback(g_lineValue, 100, delay);
+  //   }
+  // }
 
-  // haptic feedback, increasing intensity with position
-  if (g_tactileEnabled &&  g_lineValue % g_click == 0) {
-    const auto intensity = map(g_lineValue, 0, LINE_MAX, 1, g_maxClickIntensity);
-    faderMover.tick(intensity);
-  }
+  // // haptic feedback, increasing intensity with position
+  // if (g_tactileEnabled &&  g_lineValue % g_click == 0) {
+  //   const auto intensity = map(g_lineValue, 0, LINE_MAX, 1, g_maxClickIntensity);
+  //   faderMover.tick(intensity);
+  // }
 
-  if (Serial.available() > 0) {
-    const auto read_value = Serial.parseInt();
-    P_LBL("move to: ", read_value);
-    faderMover.moveToNorm(read_value);
+  // if (Serial.available() > 0) {
+  //   const auto read_value = Serial.parseInt();
+  //   P_LBL("move to: ", read_value);
+  //   faderMover.moveToNorm(read_value);
 
-    // if (read_value >= 0) {
-    //   // const auto position = constrain(read_value, 0, ANALOG_IN_MAX);
-    //   P_LBL("training: ", read_value);
-    //   touche.startTraining(read_value);
-    //   g_toucheTraining = true;
-    // }
-    // else {
-    //   touche.setLag(abs(read_value));
-    // }
-  }
+  //   // if (read_value >= 0) {
+  //   //   // const auto position = constrain(read_value, 0, ANALOG_IN_MAX);
+  //   //   P_LBL("training: ", read_value);
+  //   //   touche.startTraining(read_value);
+  //   //   g_toucheTraining = true;
+  //   // }
+  //   // else {
+  //   //   touche.setLag(abs(read_value));
+  //   // }
+  // }
 }
